ec7968e5946f32c9012b1894a2fe46c9dffd7dd0
Pedro Santos <pedro.santos.personal@gmail.com>
2024-01-29 23:07:12 +0100

    test: E2E starter tests

--- E2E/example.spec.ts
+++ E2E/example.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://harrisjazzlines.com//');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Barry Harris Line generation App/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://harrisjazzlines.com/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'C' }).click();
+  await page.getByRole('link', { name: 'Dominant' }).click();
+  await page.getByRole('link', { name: 'C Position' }).click();
+  await page.getByRole('link', { name: 'Arpeggio Up' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
