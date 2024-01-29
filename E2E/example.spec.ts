4ff08a60f6c80e7cc764a8ef5d34c08e175f594e
Pedro Santos <pedro.santos.personal@gmail.com>
2024-01-29 23:18:00 +0100

    test: improve E2E selectors

--- E2E/example.spec.ts
+++ E2E/example.spec.ts
@@ -9,10 +9,10 @@ test('has title', async ({ page }) => {
 test('get started link', async ({ page }) => {
   await page.goto('https://harrisjazzlines.com/');
 
-  await page.getByRole('link', { name: 'C' }).click();
-  await page.getByRole('link', { name: 'Dominant' }).click();
-  await page.getByRole('link', { name: 'C Position' }).click();
-  await page.getByRole('link', { name: 'Arpeggio Up' }).click();
+  await page.getByRole('link', { name: 'C', exact: true }).click();
+  await page.getByRole('link', { name: 'Dominant', exact: true }).click();
+  await page.getByRole('link', { name: 'C Position', exact: true }).click();
+  await page.getByRole('link', { name: 'Arpeggio Up', exact: true }).click();
 
   // Expects page to have a heading with the name of Installation.
   // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
--- src/LeftSide.tsx
+++ src/LeftSide.tsx
@@ -5,65 +5,62 @@ function LeftSide() {
         <b>Select Pitch</b>
       </div>
       <ul>
-          <li>
-            <a href="#">C</a>
-          </li>
-          <li>
-            <a href="#">C#</a>
-            <a href="#">Db</a>
-          </li>
-          <li>
-            <a href="#">D</a>
-          </li>
-          <li>
-            <a href="#">D#</a>
-            <a href="#">Eb</a>
-          </li>
-          <li>
-            <a href="#">E</a>
-          </li>
-          <li>
-            <a href="#">F</a>
-          </li>
-          <li>
-            <a href="#">F#</a>
-            <a href="#">Gb</a>
-          </li>
-          <li>
-            <a href="#">G</a>
-          </li>
-          <li>
-            <a href="#">G#</a>
-            <a href="#">Ab</a>
-          </li>
-          <li>
-            <a href="#">A</a>
-          </li>
-          <li>
-            <a href="#">A#</a>
-            <a href="#">Bb</a>
-          </li>
-          <li>
-            <a href="#">B</a>
-          </li>
-          <li>
-            <a href="#">C</a>
-          </li>
-        </ul>
+        <li>
+          <a href="#">C</a>
+        </li>
+        <li>
+          <a href="#">C#</a>
+          <a href="#">Db</a>
+        </li>
+        <li>
+          <a href="#">D</a>
+        </li>
+        <li>
+          <a href="#">D#</a>
+          <a href="#">Eb</a>
+        </li>
+        <li>
+          <a href="#">E</a>
+        </li>
+        <li>
+          <a href="#">F</a>
+        </li>
+        <li>
+          <a href="#">F#</a>
+          <a href="#">Gb</a>
+        </li>
+        <li>
+          <a href="#">G</a>
+        </li>
+        <li>
+          <a href="#">G#</a>
+          <a href="#">Ab</a>
+        </li>
+        <li>
+          <a href="#">A</a>
+        </li>
+        <li>
+          <a href="#">A#</a>
+          <a href="#">Bb</a>
+        </li>
+        <li>
+          <a href="#">B</a>
+        </li>
+      </ul>
       <div>
         <b>Select Scale</b>
       </div>
       <ul>
-          <li>
-            <a href="#">Major</a>
-          </li>
-          <li>
-            <a href="#">Dominant</a>
-          </li>
-          <li>
-            <a href="#">Minor</a>
-          </li>
-        </ul>
+        <li>
+          <a href="#">Major</a>
+        </li>
+        <li>
+          <a href="#">Dominant</a>
+        </li>
+        <li>
+          <a href="#">Minor</a>
+        </li>
+      </ul>
     </div>
   );
 }
