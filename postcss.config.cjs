18c3772700db4e1405bbf7e7f3c9655027679b57
Pedro Santos <pedromsantos@users.noreply.github.com>
2025-03-14 12:31:52 +0100

    chore: refine CSS configuration and styling updates
    - Updated autoprefixer configuration in postcss.config.cjs to use 'overrideBrowserslist' for improved browser compatibility.
    - Modified index.css to enhance styling by applying border color using a CSS variable, ensuring better maintainability and consistency in design.
    
    These changes improve the overall styling configuration and maintainability of the CSS files.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     tailwindcss: require.resolve('tailwindcss'),
     autoprefixer: {
-      browsers: ['last 2 versions', '> 1%', 'not dead'],
+      overrideBrowserslist: ['last 2 versions', '> 1%', 'not dead'],
     },
   },
 };
